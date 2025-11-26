2fdc2c5d064f71a349ca6964e315f4f85dd21765
Kevin Kalpala <110194653+KevinKalpala@users.noreply.github.com>
2025-11-26 07:43:00 +0000

    13th commit of Chap5

--- Chap5_DecisionMaking_Branching/5_7.c
+++ Chap5_DecisionMaking_Branching/5_7.c
@@ -17,31 +17,84 @@
  * -----------------------------------------------------------------------------------------------------------------------------
  */
 
+// #include<stdio.h>
+
+// int main()
+// {
+//     // (a) Original Floyd's triangle
+//     int count = 1;
+//     int nextStartValue = 1;
+//     for(int i = 1; i <= 91; i++)
+//     {
+//         int lastColValue = 0;
+//         for(int col = 0; col < count; col++)
+//         {
+//             printf("%d", nextStartValue+col);
+//             lastColValue = col;
+//         }
+//         printf("\n");
+
+//         nextStartValue += lastColValue;
+//         count++;
+
+//         if(nextStartValue > 91)
+//         {
+//             break;
+//         }
+//     }
+
+//     return 0;
+// }
+
 #include<stdio.h>
+
+int getIntData(int minLimit);
 
 int main()
 {
-    // (a) Original Floyd's triangle
-    int count = 1;
-    int nextStartValue = 1;
-    for(int i = 1; i <= 91; i++)
+    int maxcount;
+    int row = 1, printedInRow = 0;
+    
+    printf("Please enter the upper limit to print: ");
+    maxcount = getIntData(1);
+    
+    for(int count = 1; count <= maxcount; count++)
     {
-        int lastColValue = 0;
-        for(int col = 0; col < count; col++)
+        printf("%-3d", count);
+        printedInRow++;
+        if (printedInRow == row)
         {
-            printf("%d", nextStartValue+col);
-            lastColValue = col;
-        }
-        printf("\n");
-
-        nextStartValue += lastColValue;
-        count++;
-
-        if(nextStartValue > 91)
-        {
-            break;
+            printf("\n");
+            row++;
+            printedInRow = 0;
         }
     }
-
+    
     return 0;
+}
+
+int getIntData(int minLimit)
+{
+    int val;
+
+    while(1)
+    {
+        if (scanf("%d", &val) != 1)
+        {
+            printf("Error: Input could not be read as an integer.\n");
+            while(getchar() != '\n' && !feof(stdin));
+            continue;
+        }
+
+        if (val < minLimit)
+        {
+            printf("Enter an integer >= %d\n", minLimit);
+            continue;
+        }
+
+        else
+        {
+            return val;
+        }
+    }
 }

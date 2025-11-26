44a153a4077fbdbd923022b71970f77d03e261d2
Kevin Kalpala <110194653+KevinKalpala@users.noreply.github.com>
2025-11-26 08:18:58 +0000

    14th commit of Chap5

--- Chap5_DecisionMaking_Branching/5_7.c
+++ Chap5_DecisionMaking_Branching/5_7.c
@@ -17,34 +17,62 @@
  * -----------------------------------------------------------------------------------------------------------------------------
  */
 
+// ********************       (a) Floyd's triangle      *************************
+
 // #include<stdio.h>
+
+// int getIntData(int minLimit);
 
 // int main()
 // {
-//     // (a) Original Floyd's triangle
-//     int count = 1;
-//     int nextStartValue = 1;
-//     for(int i = 1; i <= 91; i++)
+//     int maxcount;
+//     int row = 1, printedInRow = 0;
+    
+//     printf("Please enter the upper limit to print: ");
+//     maxcount = getIntData(1);
+    
+//     for(int count = 1; count <= maxcount; count++)
 //     {
-//         int lastColValue = 0;
-//         for(int col = 0; col < count; col++)
+//         printf("%-3d", count);
+//         printedInRow++;
+//         if (printedInRow == row)
 //         {
-//             printf("%d", nextStartValue+col);
-//             lastColValue = col;
-//         }
-//         printf("\n");
-
-//         nextStartValue += lastColValue;
-//         count++;
-
-//         if(nextStartValue > 91)
-//         {
-//             break;
+//             printf("\n");
+//             row++;
+//             printedInRow = 0;
 //         }
 //     }
-
+    
 //     return 0;
 // }
+
+// int getIntData(int minLimit)
+// {
+//     int val;
+    
+//     while(1)
+//     {
+//         if (scanf("%d", &val) != 1)
+//         {
+//             printf("Error: Input could not be read as an integer.\n");
+//             while(getchar() != '\n' && !feof(stdin));
+//             continue;
+//         }
+        
+//         else if (val < minLimit)
+//         {
+//             printf("Enter an integer >= %d\n", minLimit);
+//             continue;
+//         }
+        
+//         else
+//         {
+//             return val;
+//         }
+//     }
+// }
+
+// ********************       (a) Modified Floyd's triangle      *************************
 
 #include<stdio.h>
 
@@ -53,20 +81,22 @@ int getIntData(int minLimit);
 int main()
 {
     int maxcount;
-    int row = 1, printedInRow = 0;
+    int row = 1, col = 0;
     
-    printf("Please enter the upper limit to print: ");
+    printf("Please enter the number of terms to print: ");
     maxcount = getIntData(1);
-    
+
     for(int count = 1; count <= maxcount; count++)
     {
-        printf("%-3d", count);
-        printedInRow++;
-        if (printedInRow == row)
+        printf("%d ", ( row + col ) % 2);
+
+        col++;
+
+        if (col == row)
         {
             printf("\n");
             row++;
-            printedInRow = 0;
+            col = 0;
         }
     }
     
@@ -75,18 +105,18 @@ int main()
 
 int getIntData(int minLimit)
 {
-    int val;
+    int temp;
 
     while(1)
     {
-        if (scanf("%d", &val) != 1)
+        if (scanf("%d", &temp) != 1)
         {
-            printf("Error: Input could not be read as an integer.\n");
+            printf("Error: Input could not be read as an integer\n");
             while(getchar() != '\n' && !feof(stdin));
             continue;
         }
-
-        if (val < minLimit)
+        
+        else if (temp < minLimit)
         {
             printf("Enter an integer >= %d\n", minLimit);
             continue;
@@ -94,7 +124,7 @@ int getIntData(int minLimit)
 
         else
         {
-            return val;
+            return temp;
         }
     }
 }

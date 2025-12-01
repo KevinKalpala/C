7c9b57e1595f44cd8f649d75a5852804b781506a
Kevin Kalpala <110194653+KevinKalpala@users.noreply.github.com>
2025-12-01 06:10:43 +0000

    19th commit of Chap5

--- Chap5_DecisionMaking_Branching/5_10.c
+++ Chap5_DecisionMaking_Branching/5_10.c
@@ -1,19 +1,30 @@
 /**
  * ---------------------------------------------------------------------------------------------------------------------------
- * Program written by : Kevin Kalpala
- * Date               : 28 / 11 / 25
- * 
+ * Program Written By : Kevin Kalpala
+ * Date               : 01 / 12 / 25
+ *
  * Description:
- * 
- * 
+ *      This program calculates the real roots of a quadratic equation of the form:
+ *                      ax^2 + bx + c = 0
+ *      The program takes input from the user for the coefficients a, b, and c.
+ *      Based on the input values, the program determines whether:
+ *          - No solution exists (if both a and b are zero)
+ *          - Only one real root exists (if a = 0)
+ *          - No real roots exist (if the discriminant is negative)
+ *          - Two real roots exist (if the discriminant is zero or positive)
+ *
  * Input:
- * 
- * 
+ *      Integer values of a, b, and c entered by the user.
+ *
  * Output:
- * 
- * 
+ *      Displays one of the following based on the input:
+ *          - "No solution exists"
+ *          - One real root (linear equation)
+ *          - "No real roots exist"
+ *          - Two real roots (quadratic equation)
+ *
  * Note:
- * 
+ *      Input validation is included to prevent invalid data entry.
  * ---------------------------------------------------------------------------------------------------------------------------
  */
 
@@ -30,10 +41,10 @@ int main()
     double x1, x2;
     
     printf("\n****** Welcome User! ******\n");
-    printf("Please enter the quadratic equation (in form [ax2 + bx + c = 0]): ");
+    printf("Please enter the values of a, b and c : ");
     getValues(&a, &b, &c);
 
-    printf("The equation is %dx2 + %dx + %d = 0\n", a, b, c);
+    printf("The equation is %dx^2 + %dx + %d = 0\n", a, b, c);
     
     calculateRoots(a, b, c);
     
@@ -44,7 +55,7 @@ void getValues(int* a, int* b, int* c)
 {
     while(1)
     {
-        if (scanf("%dx2 +%dx +%d = 0", a, b, c) != 3)
+        if (scanf("%d %d %d", a, b, c) != 3)
         {
             printf("Error: Invalid input. Please try again...........\n");
             while(getchar() != '\n' && !feof(stdin));
@@ -59,10 +70,16 @@ void getValues(int* a, int* b, int* c)
 
 void calculateRoots(int a, int b, int c)
 {
-    int d = (( pow (b, 2) ) - ( 4 * a * c ));
+    double d = (( pow (b, 2) ) - ( 4 * (double)a * c ));
     if (a == 0 && b == 0)
     {
         printf("No solution exists for this equation because a = 0 and b = 0\n");
+    }
+    else if (a == 0)
+    {
+        double x = (( (-1) * c ) / b);
+        printf("Only one real root of this equation\n");
+        printf("The root is  x = %.2lf", x);
     }
     else if (d < 0)
     {
@@ -70,22 +87,13 @@ void calculateRoots(int a, int b, int c)
     }
     else
     {
-        if (a == 0)
-        {
-            double x = (( (-1) * c ) / b);
-            printf("Only one real root of this equation\n");
-            printf("The root is  x = %.2lf", x);
-        }
-        else
-        {
-            double x1, x2;
-            
-            x1 = ((-1) * b) + ((sqrt((double)d)) / (2 * a));
-            x2 = ((-1) * b) - ((sqrt((double)d)) / (2 * a));
+        double x1, x2;
+        
+        x1 = ((-1 * b) + (sqrt(d))) / (2 * a);
+        x2 = ((-1 * b) - (sqrt(d))) / (2 * a);
 
-            printf("Two real roots of this equation exists and are given below\n");
-            printf("x1 = %.2lf\n", x1);
-            printf("x2 = %.2lf\n", x2);
-        }
+        printf("Two real roots of this equation exists and are given below\n");
+        printf("x1 = %.2lf\n", x1);
+        printf("x2 = %.2lf\n", x2);
     }
 }

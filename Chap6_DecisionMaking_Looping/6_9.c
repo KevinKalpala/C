ea63ac52655363c90fa25cac6b5253d999a4667e
Kevin Kalpala <110194653+KevinKalpala@users.noreply.github.com>
2025-12-10 10:28:39 +0000

    12th commit of Chap6

--- Chap6_DecisionMaking_Looping/6_9.c
+++ Chap6_DecisionMaking_Looping/6_9.c
@@ -4,16 +4,26 @@
  * Date               : 10 / 12 / 25
  * 
  * Description:
- * 
+ *   This program prints a horizontal 2D ASCII graph of two exponential functions:
+ *   y1 = 50 * e^(-a*x)
+ *   y2 = 50 * e^(-(a*x*x)/2)
+ *   It compares both values at each x-step and prints a visual representation 
+ *   using different characters (#, *, 0, -) to show their relative positions.
  * 
  * Input:
- * 
+ *   No input is taken from the user. The program uses:
+ *     - a = 0.4
+ *     - x ranges from 0 to 5 in steps of 0.25
  * 
  * Output:
- * 
+ *   A horizontally plotted ASCII graph where:
+ *     - "#" marks the point where y1 and y2 overlap
+ *     - "*" marks y2 and "0" marks y1 when they differ
+ *     - "-" is used to draw the gap between the two function values
  * 
  * Note:
- * 
+ *   The graph displays Y-values horizontally and X-values vertically.
+ *   The x = 2.5 position is marked on the graph with an "X" indicator.
  * -------------------------------------------------------------------------------------------------------------------------------
  */
 

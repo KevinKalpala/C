bfbb48c3ebe481cdacf00e389bbc44f42b266096
Kevin Kalpala <110194653+KevinKalpala@users.noreply.github.com>
2026-01-06 08:29:08 +0000

    18th commit of Chap7

--- Chap7_Arrays/7_13.c
+++ Chap7_Arrays/7_13.c
@@ -21,6 +21,8 @@
 
 int getDimension(char* prompt);
 
+void getMatrix(char* prompt, int m, int n, int arr[m][n]);
+
 int main()
 {
     int m, n;
@@ -30,13 +32,17 @@ int main()
     m = getDimension("Please enter the value of m : ");
     n = getDimension("Please enter the value of n : ");
     
+    int matrix[m][n];
+    
+    getMatrix("Please enter the details of the matrix :-\n", m, n, matrix);
+    
     return 0;
 }
 
 int getDimension(char* prompt)
 {
     int data;
-
+    
     while(1)
     {
         printf("%s", prompt);
@@ -57,5 +63,32 @@ int getDimension(char* prompt)
         {
             return data;
         }
+    }
+}
+
+void getMatrix(char* prompt, int m, int n, int arr[m][n])
+{
+    printf("%s", prompt);
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            while(1)
+            {
+                if (scanf("%d ", arr[i][j]) != 1)
+                {
+                    printf("Error: Value could not be read as an integer.\n");
+                    printf("Please try again...........\n");
+                    while(getchar() != '\n' && !feof(stdin));
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        printf("\n");
     }
 }

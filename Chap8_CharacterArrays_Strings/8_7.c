cd5b79b8012b2351ef3724c46d47b6aa5cb07b86
Kevin Kalpala <110194653+KevinKalpala@users.noreply.github.com>
2026-02-20 06:03:56 +0000

    9th commit of Chap8

---
+++ Chap8_CharacterArrays_Strings/8_7.c
@@ -0,0 +1,39 @@
+/**
+ * --------------------------------------------------------------------------------------------
+ * Program written by : Kevin Kalpala
+ * Date               : 20 / 02 / 26
+ * 
+ * Description:
+ * 
+ * 
+ * Input:
+ * 
+ * 
+ * Output:
+ * 
+ * 
+ * Note:
+ * 
+ * --------------------------------------------------------------------------------------------
+ */
+
+#include<stdio.h>
+
+#define MAXSIZE 4
+
+int main()
+{
+    char vehicleType[MAXSIZE][20];
+    char monthOfSales[MAXSIZE][5];
+    int price[MAXSIZE];
+
+    for(int i = 0; i < maxsize; i++)
+    {
+        printf("Please enter the vehicle type")
+        fgets(vehicleType[i], 20, stdin);
+        vehicleType[i][strcspn(vehicleType[i], "\n")]
+
+    }
+
+    return 0;
+}
